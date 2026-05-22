faf1ff1f2166d59c08914cee06529ffa757e1c92
Alishlah <alishlah56@gmail.com>
2026-05-22 14:46:17 +0700

    first commit

---
+++ catatan.py
@@ -0,0 +1,8 @@
+# 1. Matikan venv lokal yang aktif saat ini
+# " deactivate "
+
+# 2. Aktifkan lingkungan conda yang sudah lengkap library-nya
+# " conda activate space_gesture "
+
+# 3. Jalankan kodenya
+# " python index.py "
